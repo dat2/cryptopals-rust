54e4cff2542ff2dd6de440d5acc8ee0822c68779
Nicholas Dujay <nickdujay@gmail.com>
2017-09-23 23:19:21 -0400

    finish challenge 5

--- src/lib.rs
+++ src/lib.rs
@@ -196,6 +196,11 @@ pub fn detect_single_character_xor(xor_bytes: Vec<Vec<u8>>) -> Vec<u8> {
     .unwrap()
 }
 
+fn encrypt_repeating_key(input_bytes: &[u8], key: &[u8]) -> Vec<u8> {
+  let repeated_key: Vec<_> = key.iter().cycle().cloned().take(input_bytes.len()).collect();
+  fixed_xor(input_bytes, &repeated_key)
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -220,5 +225,16 @@ mod tests {
     let b_bytes = from_hex_string("686974207468652062756c6c277320657965").unwrap();
     let actual = fixed_xor(&a_bytes, &b_bytes);
     assert_eq!(actual, expected);
+  }
+
+  // challenge 5
+  #[test]
+  fn test_encrypt_repeating_key() {
+    let expected = from_hex_string("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f").unwrap();
+
+    let input_string = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+    let key = "ICE";
+    let actual = encrypt_repeating_key(input_string.as_bytes(), key.as_bytes());
+    assert_eq!(expected, actual);
   }
 }
